201755a9a04959c04bcc1f42372de456fa04f565
MikeF <mike_forster@yahoo.co.uk>
2024-12-13 13:32:29 +0000

    Tidy up some stuff

--- GMap.NET/GMap.NET.WindowsForms/GMapControl.cs
+++ GMap.NET/GMap.NET.WindowsForms/GMapControl.cs
@@ -616,7 +616,7 @@ namespace GMap.NET.WindowsForms
 
         static GMapControl()
         {
-            Trace.Listeners.Clear(); // MDF added
+            //Trace.Listeners.Clear();
             if (!IsDesignerHosted)
             {
                 GMapImageProxy.Enable();
